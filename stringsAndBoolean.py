de6b24a6733dbb8ef1789643bf94903f229b3e54
sudesh.saini <sudesh.saini@gmail.com>
2020-08-09 00:11:24 +0530

    string and boolean

---
+++ stringsAndBoolean.py
@@ -0,0 +1,26 @@
+
+#there is no need to declare type of the variable
+
+name = 'sudesh' #single quotes are fine
+surname = "saini" # so are double quotes
+#concate multiple strings
+fullname = name + ' ' + surname
+
+
+print (name)
+print (surname)
+
+print (fullname)
+
+
+# boolean - True and Flase
+# its case sensitive, true is not same as True
+#also a keyword 'not' to reverse the switch
+
+status = True
+newstatus  =  not True
+
+if (status):
+    print('its true')
+if (newstatus):
+    print('wont print as its false')
